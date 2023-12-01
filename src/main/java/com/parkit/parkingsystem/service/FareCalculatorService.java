a166296d97333f2d54766a82209b60228ce020e2
theryeddari <121760376+theryeddari@users.noreply.github.com>
2023-12-01 10:36:20 +0100

    modification getHours by getTime() and conversion to decimal hours.

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,19 +10,18 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        long inMillisecond = ticket.getInTime().getTime();
+        long outMillisecond = ticket.getOutTime().getTime();
+        long durationMillisecond = outMillisecond - inMillisecond;
+        float durationHourDecimal = ((float) durationMillisecond / (60 * 60 * 1000));
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(durationHourDecimal * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(durationHourDecimal * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
