e50f6689c6ebec2a7ac579cea25d8a03aaaeb04c
theryeddari <121760376+theryeddari@users.noreply.github.com>
2023-12-03 14:59:21 +0100

    Completed TODO 2 for testParkingLotExitRecurringUser(): refactor and deleted Exception never thrown methods

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -20,9 +20,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Date;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,7 +35,7 @@ public class ParkingDataBaseIT {
     private static InputReaderUtil inputReaderUtil;
 
     @BeforeAll
-    public static void setUp() throws Exception{
+    public static void setUp() {
         parkingSpotDAO = new ParkingSpotDAO();
         parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
         ticketDAO = new TicketDAO();
@@ -46,52 +44,67 @@ public class ParkingDataBaseIT {
     }
 
     @BeforeEach
-    public void setUpPerTest() throws Exception {
+    public void setUpPerTest() {
         when(inputReaderUtil.readSelection()).thenReturn(1);
         when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
         dataBasePrepareService.clearDataBaseEntries();
     }
 
     @AfterAll
-    public static void tearDown(){
+    public static void tearDown() {
 
     }
 
-    @Test
-    void testParkingACar(){
+    private static void customerJourney(int choice) {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
+
+        switch (choice) {
+            case 1:
+                parkingService.processIncomingVehicle();
+                break;
+            case 2:
+                parkingService.processIncomingVehicle();
+                Awaitility.await().pollDelay(1, SECONDS).until(() -> true);
+                parkingService.processExitingVehicle();
+                break;
+            case 3:
+                for (int i = 0; i < 2; i++) {
+                    parkingService.processIncomingVehicle();
+                    Awaitility.await().pollDelay(1, SECONDS).until(() -> true);
+                    parkingService.processExitingVehicle();
+                }
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + choice);
+        }
+    }
+
+    @Test
+    void testParkingACar() {
+        customerJourney(1);
         Ticket checkTicket = ticketDAO.getTicket("ABCDEF");
         assertNotNull(checkTicket);
         assertFalse(checkTicket.getParkingSpot().isAvailable());
     }
 
     @Test
-    void testParkingLotExit(){
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
-        Awaitility.await().pollDelay(1, SECONDS).until(() -> true);
-        parkingService.processExitingVehicle();
+    void testParkingLotExit() {
+        customerJourney(2);
 
         Ticket checkTicket = ticketDAO.getTicket("ABCDEF");
 
         assertNotNull(checkTicket.getOutTime());
         long timeOut = checkTicket.getOutTime().getTime();
         long nowTime = new Date().getTime();
-        assertEquals(nowTime, timeOut,(1000)); // 1 secondes de délai max connexion a la base etc.
+        assertEquals(nowTime, timeOut, (1000)); // 1 secondes de délai max connexion a la base etc.
         double gapTimeDecimal = (double) (checkTicket.getOutTime().getTime() - checkTicket.getInTime().getTime()) / (60 * 60 * 1000);
         double priceExpected = gapTimeDecimal * Fare.CAR_RATE_PER_HOUR;
-        assertEquals(priceExpected,checkTicket.getPrice(),0.1);
+        assertEquals(priceExpected, checkTicket.getPrice(), 0.1);
     }
+
     @Test
     void testParkingLotExitRecurringUser() {
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
-        await().pollDelay(5, SECONDS).until(() -> true);
-        parkingService.processExitingVehicle();
-        parkingService.processIncomingVehicle();
-        await().pollDelay(5, SECONDS).until(() -> true);
-        parkingService.processExitingVehicle();
+        customerJourney(3);
 
         Ticket checkTicket = ticketDAO.getTicket("ABCDEF");
 
@@ -99,7 +112,6 @@ public class ParkingDataBaseIT {
         assertNotNull(checkTicket.getOutTime());
         double gapTimeDecimal = (double) (checkTicket.getOutTime().getTime() - checkTicket.getInTime().getTime()) / (60 * 60 * 1000);
         double priceExpected = gapTimeDecimal * Fare.CAR_RATE_PER_HOUR * 0.95;
-        assertEquals(priceExpected,checkTicket.getPrice(),0.1);
-        //TODO : refactor ParkingDataBaseIT
+        assertEquals(priceExpected, checkTicket.getPrice(), 0.1);
     }
 }
