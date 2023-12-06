d73ecf4f5add044eda2b192dcb4c705391f7376a
theryeddari <121760376+theryeddari@users.noreply.github.com>
2023-12-06 18:42:53 +0100

    refactor with try with resource, delete condition (specification JDBC and mysql), modify boolean useless by void

--- src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
@@ -33,23 +33,15 @@ public class ParkingSpotDAO {
         }
     }
 
-    public boolean updateParking(ParkingSpot parkingSpot){
+    public void updateParking(ParkingSpot parkingSpot) {
         //update the availability fo that parking slot
-        Connection con = null;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT);
+        try (Connection con = dataBaseConfig.getConnection();
+             PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT)) {
             ps.setBoolean(1, parkingSpot.isAvailable());
             ps.setInt(2, parkingSpot.getId());
-            int updateRowCount = ps.executeUpdate();
-            dataBaseConfig.closePreparedStatement(ps);
-            return (updateRowCount == 1);
-        }catch (Exception ex){
-            logger.error("Error updating parking info",ex);
-            return false;
-        }finally {
-            dataBaseConfig.closeConnection(con);
+            ps.executeUpdate();
+        } catch (Exception ex) {
+            logger.error("Error updating parking info", ex);
         }
     }
-
 }
