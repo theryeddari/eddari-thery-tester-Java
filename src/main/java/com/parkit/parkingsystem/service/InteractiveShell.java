aa87d656f447aa8711e31e50c4a7856a1f06ce48
theryeddari <121760376+theryeddari@users.noreply.github.com>
2023-12-09 00:42:21 +0100

    add private constructor to hide the implicit one

--- src/main/java/com/parkit/parkingsystem/service/InteractiveShell.java
+++ src/main/java/com/parkit/parkingsystem/service/InteractiveShell.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class InteractiveShell {
+    private InteractiveShell() {}
 
     private static final Logger logger = LogManager.getLogger("InteractiveShell");
 
