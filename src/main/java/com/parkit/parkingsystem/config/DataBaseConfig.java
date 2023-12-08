799db3cc18fc1f40b39c2211b5bdfc521abdd84c
theryeddari <121760376+theryeddari@users.noreply.github.com>
2023-12-09 00:50:25 +0100

    deleted method 'closePreparedStatement(java.sql.PreparedStatement) and 'closeResultSet(java.sql.ResultSet)' because we modify to try with resource in commit d73ecf4F et c1e96a96

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -26,26 +26,4 @@ public class DataBaseConfig {
             }
         }
     }
-
-    public void closePreparedStatement(PreparedStatement ps) {
-        if(ps!=null){
-            try {
-                ps.close();
-                logger.info("Closing Prepared Statement");
-            } catch (SQLException e) {
-                logger.error("Error while closing prepared statement",e);
-            }
-        }
-    }
-
-    public void closeResultSet(ResultSet rs) {
-        if(rs!=null){
-            try {
-                rs.close();
-                logger.info("Closing Result Set");
-            } catch (SQLException e) {
-                logger.error("Error while closing result set",e);
-            }
-        }
-    }
 }
